48056bb2693ae456126afec567448dc30fae0b2a
helysousa <hely.sousa@outlook.com>
2016-07-06 08:19:02 -0400

    Minha primeira aplicação

---
+++ src/App/Action/TestePageAction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Action;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
+use Zend\Expressive\Router;
+use Zend\Expressive\Template;
+use Zend\Expressive\Plates\PlatesRenderer;
+use Zend\Expressive\Twig\TwigRenderer;
+use Zend\Expressive\ZendView\ZendViewRenderer;
+
+class TestePageAction
+{
+    private $router;
+
+    private $template;
+
+    public function __construct(Router\RouterInterface $router, Template\TemplateRendererInterface $template = null)
+    {
+        $this->router   = $router;
+        $this->template = $template;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
+    {
+        return new HtmlResponse($this->template->render('app::teste-page'));
+    }
+}
